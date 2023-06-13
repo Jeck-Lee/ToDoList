96edb844662c70ffe9ce5a428d73975639c266f7
Jeck-Lee <jecklee02@gmail.com>
2023-06-13 09:55:18 +0900

    first commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jeck Lee on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
