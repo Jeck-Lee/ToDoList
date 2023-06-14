3c809d9cd7e1387d89b3c7b5b8739a9aa5056446
Jeck-Lee <jecklee02@gmail.com>
2023-06-14 22:48:33 +0900

    Add List

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import RealmSwift
+
+struct ToDoCell: View {
+	var item: ToDoItem
+	
+	var body: some View {
+		HStack {
+			Text(item.content)
+				.foregroundColor(item.isComplete ? .gray : .black)
+			Spacer()
+			Button(action: {
+				try! Realm().write {
+					item.isComplete.toggle()
+				}
+			}) {
+				Image(item.isComplete ? "check_box-check" :  "check_box")
+			}
+		}
+	}
+}
 
 struct ContentView: View {
+	let vm = ToDoVM()
+	
+	init() {
+		vm.getItems()
+	}
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		List(vm.items, id: \.self) { item in
+			ToDoCell(item: item)
+		}
     }
 }
 
